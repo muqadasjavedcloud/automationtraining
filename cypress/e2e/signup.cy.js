77e12ca74d93a704c9512c435193040af96dd726
muqadasjaved <muqadasjaved101@mail.com>
2025-05-27 13:57:56 +0500

    check workflow run after chnges

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -6,7 +6,7 @@ cy.visit('https://pdf-qa.valortech.co.mz');
  
 cy.get('.loginLink').click();
 cy.url().should('include', '/auth/sign-up'); 
- cy.get('#email').type('muqadasjaved101+test23@gmail.com');
+ cy.get('#email').type('muqadasjaved101+test234@gmail.com');
  cy.get('#password').type('Qaaa@2222');
  cy.get('#c_password').type('Qaaa@2222');
  cy.get("input[type='checkbox']").check(); //check box 
@@ -14,9 +14,9 @@ cy.url().should('include', '/auth/sign-up');
   cy.get('#company-type').click()
   cy.get('li').contains('Corporate').click();      //dropdown with select option 
   // cy.get('#company-type').should('contain', 'Corporate');//
- cy.get('input[name="Company Name"]').type('dm11 company');
- cy.get('input[name="NUIT Number"]').type('254622035');
- cy.get('input[name="Commercial Registration Number"]').type('62593239523');
+ cy.get('input[name="Company Name"]').type('dm112 company');
+ cy.get('input[name="NUIT Number"]').type('234622035');
+ cy.get('input[name="Commercial Registration Number"]').type('63593239523');
  cy.get('input[name="Company Address"]').type('lahoree street 11');
  cy.contains('Submit').click();
  cy.get('.logout-btn').click();
