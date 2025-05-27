a14877215833fd5ffac04368ce9fe1e498f0b03d
muqadasjaved <muqadasjaved101@mail.com>
2025-05-27 13:54:38 +0500

    check workflow run after chnges

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -14,23 +14,21 @@ cy.url().should('include', '/auth/sign-up');
   cy.get('#company-type').click()
   cy.get('li').contains('Corporate').click();      //dropdown with select option 
   // cy.get('#company-type').should('contain', 'Corporate');//
- cy.get('input[name="Company Name"]').type('dm1 company');
- cy.get('input[name="NUIT Number"]').type('254822035');
- cy.get('input[name="Commercial Registration Number"]').type('32593239523');
+ cy.get('input[name="Company Name"]').type('dm11 company');
+ cy.get('input[name="NUIT Number"]').type('254622035');
+ cy.get('input[name="Commercial Registration Number"]').type('62593239523');
  cy.get('input[name="Company Address"]').type('lahoree street 11');
  cy.contains('Submit').click();
  cy.get('.logout-btn').click();
  
-
-
- 
 })
-it('visit to admin and accept company',()=>{
+it('visit to admin and accept company',()=>{  // we are using it block in case we are redirecting to other link 
 
-    cy.visit('https://pdf-qa-admin.valortech.co.mz/');
+    cy.visit('https://pdf-qa-admin.valortech.co.mz/'); 
   cy.get('#email').type('admin@volartech.co');
  cy.get('#password').type('Test@1234');
- cy.contains('Login').click();
+ cy.get('.css-8i4dsq > .MuiButtonBase-root').click();
+
 })
 })
 
