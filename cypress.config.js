4f4a6e75097a65b46f4237685ca831b36ca1437d
muqadasjaved <muqadasjaved101@mail.com>
2025-05-27 13:51:49 +0500

    check workflow run after chnges

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     pageLoadTimeout:30000,
-    defaultCommandTimeout:30000,
+    defaultCommandTimeout:30000, // time out we add by ourself
+    experimentalOriginDependencies:true, // we are adding it for visiting different link 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,12 +1,12 @@
 
 describe('Sign-up',()=>{
-it ('positive test case',()=>{
+it ('sign Up as main positive test case',()=>{
 cy.visit('https://pdf-qa.valortech.co.mz');
 //cy.visit('https://pdf-qa.valortech.co.mz/auth/sign-up', { failOnStatusCode: false });// //here we have to figure out why we are not diretly redirecting to sign up page 
  
 cy.get('.loginLink').click();
 cy.url().should('include', '/auth/sign-up'); 
- cy.get('#email').type('muqadasjaved101+testno2@gmail.com');
+ cy.get('#email').type('muqadasjaved101+test23@gmail.com');
  cy.get('#password').type('Qaaa@2222');
  cy.get('#c_password').type('Qaaa@2222');
  cy.get("input[type='checkbox']").check(); //check box 
@@ -14,14 +14,23 @@ cy.url().should('include', '/auth/sign-up');
   cy.get('#company-type').click()
   cy.get('li').contains('Corporate').click();      //dropdown with select option 
   // cy.get('#company-type').should('contain', 'Corporate');//
- cy.get('input[name="Company Name"]').type('mine company');
- cy.get('input[name="NUIT Number"]').type('239812095');
- cy.get('input[name="Commercial Registration Number"]').type('23941209523');
+ cy.get('input[name="Company Name"]').type('dm1 company');
+ cy.get('input[name="NUIT Number"]').type('254822035');
+ cy.get('input[name="Commercial Registration Number"]').type('32593239523');
  cy.get('input[name="Company Address"]').type('lahoree street 11');
  cy.contains('Submit').click();
+ cy.get('.logout-btn').click();
  
 
+
  
+})
+it('visit to admin and accept company',()=>{
+
+    cy.visit('https://pdf-qa-admin.valortech.co.mz/');
+  cy.get('#email').type('admin@volartech.co');
+ cy.get('#password').type('Test@1234');
+ cy.contains('Login').click();
 })
 })
 
